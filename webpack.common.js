0bd51bbb66aeb54ea3c7946fa4d52cb6edd9b883
aurelia2348 <iareliya28@gmail.com>
2025-05-20 16:10:18 +0700

    update

--- webpack.common.js
+++ webpack.common.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+mode: 'production',
   entry: path.join(__dirname, './src/scripts/app.js'),
   output: {
     path: path.join(__dirname, 'dist'),
@@ -17,7 +18,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src/index.html'), // ✅ ini penting!
+      template: path.join(__dirname, 'src/index.html'), 
       filename: 'index.html',
     }),
   ],
